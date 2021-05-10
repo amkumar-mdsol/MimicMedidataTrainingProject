b885e13d44ebd985a2b12430e1f7759824b8ee90
Neelima Joshi <njoshi@mdsol.com>
2021-05-10 10:57:57 +0530

    Active record serializer

--- app/controllers/study_groups_controller.rb
+++ app/controllers/study_groups_controller.rb
@@ -1,5 +1,7 @@
 class StudyGroupsController < ApplicationController
 
   def index
+  	@study_groups = StudyGroup.all
+  	render json: @study_groups
   end
 end
---
+++ app/serializers/study_group_serializer.rb
@@ -0,0 +1,15 @@
+class StudyGroupSerializer < ActiveModel::Serializer
+  #attributes :name, :id
+  #attributes *StudyGroup.column_names
+
+  attributes :studyGroup, :sites
+  def studyGroup
+  	object.name   #self => Serializer object, object => current json
+
+  end
+
+  def sites
+  	object.sites.map {|study| study.name.upcase }
+  end
+
+end
